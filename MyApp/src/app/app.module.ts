a29dd9b0cdbcdfbad8cc29949badf05fc4c39def
Dhekra ZELAITI <dhekra.zelaiti@gmail.com>
2020-07-13 18:49:47 +0200

    add home && detail component

--- MyApp/src/app/app.module.ts
+++ MyApp/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,7 +27,14 @@ import { HomeComponent } from './home/home.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'trainings', component: TrainingListComponent },
+      {path: 'trainings/:id', component: TrainingDetailComponent },
+      {path: 'welcome', component: HomeComponent },
+      {path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      {path: '**', redirectTo: 'welcome', pathMatch: 'full' },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
